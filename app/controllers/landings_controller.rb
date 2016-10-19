2b3178ef54d494434a27959d5099b72b8ccc2c0b
EMC <ericchesbrough@gmail.com>
2016-10-19 16:22:03 -0500

    Tattoo Text Field and Font Size are now AJAX

--- app/controllers/landings_controller.rb
+++ app/controllers/landings_controller.rb
@@ -1,4 +1,23 @@
 class LandingsController < ApplicationController
   def index
+    @tattoo = Tattoo.new
   end
+
+  def update_text
+   @updated_text = params[:tattoo][:script]
+   respond_to do |format|
+      format.js
+      format.html
+   end
+  end
+
+
+  def update_font_size
+   @updated_font_size = params[:font_size].to_s
+   respond_to do |format|
+      format.js
+      format.html
+   end
+  end
+
 end
